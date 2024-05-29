4e3c00174e6308324fc41f6eeff215fd1d319585
Aditya Karan <77965807+kanekiriaz@users.noreply.github.com>
2024-05-29 22:54:22 +0530

    Add files via upload

---
+++ RandomPassGenerator/RandomPassGenerator.js
@@ -0,0 +1,28 @@
+const passwordBox= document.getElementById('password');
+const length = 14;
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+~[]{}|<>/-=";
+
+const allChars= uppercase+lowercase+numbers+symbols;
+
+function createPassword(){
+    let password=""
+    password+= uppercase[Math.floor(Math.random()*uppercase.length)];
+    password+= lowercase[Math.floor(Math.random()*lowercase.length)];
+    password+= numbers[Math.floor(Math.random()*numbers.length)];
+    password+= symbols[Math.floor(Math.random()*symbols.length)];
+
+    while(length>password.length){
+        password+= allChars[Math.floor(Math.random()*allChars.length)];
+    }
+    passwordBox.value= password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    
+    document.execCommand("copy");
+}
+
